a02ec33c4a5e4807b11983502c17a4068cb708e6
Sunik Kupfer <kupfer@bitfire.at>
2022-02-15 10:39:00 +0100

    [WIP] test fetchLocal method - copy test file

--- app/src/androidTest/java/at/bitfire/icsdroid/CalendarFetcherTest.kt
+++ app/src/androidTest/java/at/bitfire/icsdroid/CalendarFetcherTest.kt
@@ -4,75 +4,76 @@
 
 package at.bitfire.icsdroid
 
-import android.content.ContentProviderClient
 import android.content.Context
 import android.net.Uri
-import android.provider.CalendarContract
 import android.util.Log
 import androidx.core.content.FileProvider.getUriForFile
 import androidx.test.platform.app.InstrumentationRegistry
-import at.bitfire.ical4android.MiscUtils.ContentProviderClientHelper.closeCompat
 import at.bitfire.icsdroid.Constants.TAG
 import okhttp3.MediaType
 import org.junit.*
-import org.junit.Assert.*
-import java.io.File
-import java.io.InputStream
+import java.io.*
 
 
 class CalendarFetcherTest {
 
     companion object {
 
-        private lateinit var context: Context
-        private lateinit var provider: ContentProviderClient
+        private lateinit var appContext: Context
+        private lateinit var testContext: Context
 
         @BeforeClass
         @JvmStatic
         fun setUp() {
-            context = InstrumentationRegistry.getInstrumentation().targetContext
-            provider = context.contentResolver.acquireContentProviderClient(CalendarContract.AUTHORITY)!!
-        }
-
-        @AfterClass
-        @JvmStatic
-        fun disconnect() {
-            provider.closeCompat()
+            appContext = InstrumentationRegistry.getInstrumentation().targetContext
+            testContext = InstrumentationRegistry.getInstrumentation().context
         }
 
     }
 
-    @Test
-    fun testFetchLocal_readsCorrectly() {
-        val uri = Uri.fromFile(File("sampledata/vienna-evolution.ics"))
-        Log.w(TAG, uri.toString())
-
-        val fetcher = object: CalendarFetcher(context, uri) {
-            override fun onSuccess(data: InputStream, contentType: MediaType?, eTag: String?, lastModified: Long?, displayName: String?) {
-
-                // check first few lines of stream equal actual file contents
-
-            }
-        }
-
-        fetcher.fetchLocal()
-    }
+//    @Test
+//    fun testFetchLocal_readsCorrectly() {
+//        val uri = Uri.fromFile(File("sampledata/vienna-evolution.ics"))
+//        Log.w(TAG, uri.toString())
+//
+//        val fetcher = object: CalendarFetcher(appContext, uri) {
+//            override fun onSuccess(data: InputStream, contentType: MediaType?, eTag: String?, lastModified: Long?, displayName: String?) {
+//
+//                // check first few lines of stream equal actual file contents
+//
+//            }
+//        }
+//
+//        fetcher.fetchLocal()
+//    }
 
     @Test
     fun testFetchLocal_correctDisplayName() {
 
-        // Get "content://..." uri for testfile
-        val file = File(context.filesDir,"vienna-evolution.ics")
-        val uri: Uri = getUriForFile(context, BuildConfig.APPLICATION_ID + ".test.provider", file);
+        val inputStream = testContext.assets.open("vienna-evolution.ics")
 
-        // assert display name matches
-        val fetcher = object: CalendarFetcher(context, uri) {
-            override fun onSuccess(data: InputStream, contentType: MediaType?, eTag: String?, lastModified: Long?, displayName: String?) {
-                Log.d(TAG, displayName.toString())
-                assertEquals("src/vienna-evolution.ics", displayName)
-            }
-        }
+        val file = File(appContext.filesDir, "vienna-evolution.ics")
+        val outputStream = FileOutputStream(file)
+        val uri: Uri = getUriForFile(appContext, BuildConfig.APPLICATION_ID + ".test.provider", file);
+        inputStream.copyTo(outputStream, 4096)
 
-        fetcher.run()
+        Log.v(TAG, file.absolutePath)
+        Log.w(TAG, uri.toString())
+
+//        // assert display name matches
+//        object: CalendarFetcher(context, uri) {
+//            override fun onSuccess(data: InputStream, contentType: MediaType?, eTag: String?, lastModified: Long?, displayName: String?) {
+//                Log.d(TAG, displayName.toString())
+//                assertEquals("vienna-evolution.ics", displayName)
+//            }
+//        }.run()
     }
+
+    private fun InputStream.getFilePath(dir: File, name: String, extension: String): String {
+        val file = File(dir, "$name.$extension")
+        val outputStream = FileOutputStream(file)
+        this.copyTo(outputStream, 4096)
+        return file.absolutePath
+    }
+
 }
