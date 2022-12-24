ae8519b300d0b8bc19b6f84e462dff51fba3e75e
Arnau Mora <arnyminer.z@gmail.com>
2022-12-24 15:31:44 +0100

    Deprecation
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/at/bitfire/icsdroid/db/CalendarCredentials.kt
+++ app/src/main/java/at/bitfire/icsdroid/db/CalendarCredentials.kt
@@ -5,6 +5,7 @@
 package at.bitfire.icsdroid.db
 
 import android.content.Context
+import at.bitfire.icsdroid.db.entity.Subscription
 
 class CalendarCredentials(context: Context) {
 
@@ -17,25 +18,58 @@ class CalendarCredentials(context: Context) {
 
     private val credentialPrefs = context.getSharedPreferences(PREF_CREDENTIALS, 0)
 
-
+    @Deprecated("Use Room database", replaceWith = ReplaceWith("this.get(subscription)"))
     fun get(calendar: LocalCalendar): Pair<String?, String?> {
         val url = calendar.url
         val id = calendar.id
 
         val username: String? =
-                credentialPrefs.getString("$KEY_PREFIX_USERNAME$url", null) ?:  // legacy
-                credentialPrefs.getString("$KEY_PREFIX_USERNAME$id", null)
+            credentialPrefs.getString("$KEY_PREFIX_USERNAME$url", null) ?:  // legacy
+            credentialPrefs.getString("$KEY_PREFIX_USERNAME$id", null)
 
         val password: String? =
-                credentialPrefs.getString("$KEY_PREFIX_PASSWORD$url", null) ?:  // legacy
-                credentialPrefs.getString("$KEY_PREFIX_PASSWORD$id", null)
+            credentialPrefs.getString("$KEY_PREFIX_PASSWORD$url", null) ?:  // legacy
+            credentialPrefs.getString("$KEY_PREFIX_PASSWORD$id", null)
 
         return Pair(username, password)
     }
 
+    fun get(subscription: Subscription): Pair<String?, String?> {
+        val url = subscription.url
+        val id = subscription.id
+
+        val username: String? =
+            credentialPrefs.getString("$KEY_PREFIX_USERNAME$url", null) ?:  // legacy
+            credentialPrefs.getString("$KEY_PREFIX_USERNAME$id", null)
+
+        val password: String? =
+            credentialPrefs.getString("$KEY_PREFIX_PASSWORD$url", null) ?:  // legacy
+            credentialPrefs.getString("$KEY_PREFIX_PASSWORD$id", null)
+
+        return Pair(username, password)
+    }
+
+    @Deprecated("Use Room database", replaceWith = ReplaceWith("this.put(subscription, username, password)"))
     fun put(calendar: LocalCalendar, username: String?, password: String?) {
         val prefs = credentialPrefs.edit()
         val id = calendar.id
+
+        if (username != null)
+            prefs.putString("$KEY_PREFIX_USERNAME$id", username)
+        else
+            prefs.remove("$KEY_PREFIX_USERNAME$id")
+
+        if (password != null)
+            prefs.putString("$KEY_PREFIX_PASSWORD$id", password)
+        else
+            prefs.remove("$KEY_PREFIX_PASSWORD$id")
+
+        prefs.apply()
+    }
+
+    fun put(subscription: Subscription, username: String?, password: String?) {
+        val prefs = credentialPrefs.edit()
+        val id = subscription.id
 
         if (username != null)
             prefs.putString("$KEY_PREFIX_USERNAME$id", username)
