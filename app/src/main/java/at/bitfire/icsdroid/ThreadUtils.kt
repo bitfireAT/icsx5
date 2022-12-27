b9e2a583df5511936fefa95bdc97971f6fe414e9
Arnau Mora <arnyminer.z@gmail.com>
2022-12-27 18:01:09 +0100

    Added `ViewModel.doAsync`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/at/bitfire/icsdroid/ThreadUtils.kt
+++ app/src/main/java/at/bitfire/icsdroid/ThreadUtils.kt
@@ -2,10 +2,13 @@ package at.bitfire.icsdroid
 
 import androidx.annotation.UiThread
 import androidx.annotation.WorkerThread
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
 
 /**
  * Runs the code in [block] asynchronously, in the IO thread.
@@ -23,3 +26,12 @@ fun doAsync(@WorkerThread block: suspend CoroutineScope.() -> Unit) = CoroutineS
  * @param block The block of code to be run.
  */
 suspend fun <T> ui(@UiThread block: suspend CoroutineScope.() -> T): T = withContext(Dispatchers.Main, block)
+
+/**
+ * Runs the given block of code in the IO thread using the view model scope ([ViewModel.viewModelScope]).
+ * @author Arnau Mora
+ * @since 20221227
+ * @param block The block of code to be run.
+ * @return A job that notifies about the result of the work.
+ */
+fun ViewModel.doAsync(block: suspend CoroutineScope.() -> Unit) = viewModelScope.launch(context = Dispatchers.IO, block = block)
