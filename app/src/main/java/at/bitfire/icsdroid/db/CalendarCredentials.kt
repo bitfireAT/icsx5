2acff49b888b1922395ab422d45901e0c2b3b3d0
Arnau Mora <arnyminer.z@gmail.com>
2022-12-27 18:09:52 +0100

    Removed deprecated methods
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/at/bitfire/icsdroid/db/CalendarCredentials.kt
+++ app/src/main/java/at/bitfire/icsdroid/db/CalendarCredentials.kt
@@ -18,22 +18,6 @@ class CalendarCredentials(context: Context) {
 
     private val credentialPrefs = context.getSharedPreferences(PREF_CREDENTIALS, 0)
 
-    @Deprecated("Use Room database", replaceWith = ReplaceWith("this.get(subscription)"), level = DeprecationLevel.ERROR)
-    fun get(calendar: LocalCalendar): Pair<String?, String?> {
-        val url = calendar.url
-        val id = calendar.id
-
-        val username: String? =
-            credentialPrefs.getString("$KEY_PREFIX_USERNAME$url", null) ?:  // legacy
-            credentialPrefs.getString("$KEY_PREFIX_USERNAME$id", null)
-
-        val password: String? =
-            credentialPrefs.getString("$KEY_PREFIX_PASSWORD$url", null) ?:  // legacy
-            credentialPrefs.getString("$KEY_PREFIX_PASSWORD$id", null)
-
-        return Pair(username, password)
-    }
-
     fun get(subscription: Subscription): Pair<String?, String?> {
         val url = subscription.url
         val id = subscription.id
@@ -47,24 +31,6 @@ class CalendarCredentials(context: Context) {
             credentialPrefs.getString("$KEY_PREFIX_PASSWORD$id", null)
 
         return Pair(username, password)
-    }
-
-    @Deprecated("Use Room database", replaceWith = ReplaceWith("this.put(subscription, username, password)"), level = DeprecationLevel.ERROR)
-    fun put(calendar: LocalCalendar, username: String?, password: String?) {
-        val prefs = credentialPrefs.edit()
-        val id = calendar.id
-
-        if (username != null)
-            prefs.putString("$KEY_PREFIX_USERNAME$id", username)
-        else
-            prefs.remove("$KEY_PREFIX_USERNAME$id")
-
-        if (password != null)
-            prefs.putString("$KEY_PREFIX_PASSWORD$id", password)
-        else
-            prefs.remove("$KEY_PREFIX_PASSWORD$id")
-
-        prefs.apply()
     }
 
     fun put(subscription: Subscription, username: String?, password: String?) {
