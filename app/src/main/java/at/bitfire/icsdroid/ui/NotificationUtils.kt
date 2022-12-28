64b112036ed4f4558779a3a4ac4c3ce87fc50377
Arnau Mora <arnyminer.z@gmail.com>
2022-12-28 11:14:13 +0100

    Ktdocs
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/at/bitfire/icsdroid/ui/NotificationUtils.kt
+++ app/src/main/java/at/bitfire/icsdroid/ui/NotificationUtils.kt
@@ -11,26 +11,49 @@ import android.content.Context
 import android.os.Build
 import at.bitfire.icsdroid.R
 
+/**
+ * Provides some utilities for notifications.
+ * @since 20221228
+ */
 object NotificationUtils {
 
+    /**
+     * The name of the sync channel.
+     * @since 20221228
+     */
     const val CHANNEL_SYNC = "sync"
 
     const val NOTIFY_PERMISSION = 0
 
 
+    /**
+     * Contains the [PendingIntent.FLAG_IMMUTABLE] on SDK level 23+.
+     * @since 20221228
+     */
     val flagImmutableCompat: Int =
-        if (Build.VERSION.SDK_INT >= 23)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
             PendingIntent.FLAG_IMMUTABLE
         else
             0
 
 
+    /**
+     * Creates all the required notification channels.
+     * @since 20221228
+     * @param context The context that is making the request.
+     * @return A reference to the [NotificationManager].
+     */
     fun createChannels(context: Context): NotificationManager {
         val nm = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        if (Build.VERSION.SDK_INT >= 26) {
-            nm.createNotificationChannel(NotificationChannel(CHANNEL_SYNC,
-                    context.getString(R.string.notification_channel_sync_problem), NotificationManager.IMPORTANCE_LOW))
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            nm.createNotificationChannel(
+                NotificationChannel(
+                    CHANNEL_SYNC,
+                    context.getString(R.string.notification_channel_sync_problem),
+                    NotificationManager.IMPORTANCE_LOW,
+                )
+            )
         }
 
         return nm
