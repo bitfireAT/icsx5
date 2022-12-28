0d52847b62932d66e754c628c5a7a438fff1f21a
Arnau Mora <arnyminer.z@gmail.com>
2022-12-28 16:37:11 +0100

    Fixed event population
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/at/bitfire/icsdroid/db/sync/SubscriptionAndroidEvent.kt
+++ app/src/main/java/at/bitfire/icsdroid/db/sync/SubscriptionAndroidEvent.kt
@@ -3,34 +3,47 @@ package at.bitfire.icsdroid.db.sync
 import android.content.ContentValues
 import android.content.Context
 import android.provider.CalendarContract
-import androidx.core.content.contentValuesOf
-import at.bitfire.ical4android.AndroidCalendar
-import at.bitfire.ical4android.AndroidEvent
-import at.bitfire.ical4android.AndroidEventFactory
-import at.bitfire.ical4android.Event
+import at.bitfire.ical4android.*
 import at.bitfire.icsdroid.db.entity.Subscription
+import net.fortuna.ical4j.model.DateTime
+import net.fortuna.ical4j.model.property.LastModified
 
-class SubscriptionAndroidEvent: AndroidEvent {
+class SubscriptionAndroidEvent : AndroidEvent {
     var uid: String? = null
     var lastModified = 0L
 
-    constructor(calendar: AndroidCalendar<AndroidEvent>, contentValues: ContentValues): super(calendar, contentValues)
+    private constructor(calendar: AndroidCalendar<AndroidEvent>, contentValues: ContentValues) : super(calendar, contentValues) {
+        uid = contentValues.getAsString(CalendarContract.Events._SYNC_ID)
+    }
 
-    constructor(calendar: AndroidCalendar<AndroidEvent>, event: Event): super(calendar, event)
+    constructor(calendar: AndroidCalendar<AndroidEvent>, event: Event) : super(calendar, event) {
+        uid = event.uid
+        lastModified = event.lastModified?.dateTime?.time ?: 0
+    }
 
-    constructor(context: Context, subscription: Subscription, event: Event): this(subscription.getCalendar(context), event)
+    constructor(context: Context, subscription: Subscription, event: Event) : this(subscription.getCalendar(context), event)
 
-    constructor(context: Context, subscription: Subscription, id: Long): this(subscription.getCalendar(context), id)
+    override fun populateEvent(row: ContentValues, groupScheduled: Boolean) {
+        super.populateEvent(row, groupScheduled)
 
-    @Deprecated("Try passing contentValues directly.")
-    constructor(calendar: AndroidCalendar<AndroidEvent>, id: Long): this(
-        calendar,
-        contentValuesOf(
-            CalendarContract.Events._ID to id,
-        ),
-    )
+        val event = requireNotNull(event)
+        event.uid = uid
+        event.lastModified = LastModified(DateTime(lastModified))
+    }
 
-    class Factory: AndroidEventFactory<SubscriptionAndroidEvent> {
+    override fun buildEvent(recurrence: Event?, builder: BatchOperation.CpoBuilder) {
+        super.buildEvent(
+            recurrence,
+            if (recurrence == null)
+            // master event
+                builder.withValue(CalendarContract.Events._SYNC_ID, uid)
+            else
+            // exception
+                builder.withValue(CalendarContract.Events.ORIGINAL_SYNC_ID, uid)
+        )
+    }
+
+    class Factory : AndroidEventFactory<SubscriptionAndroidEvent> {
         override fun fromProvider(calendar: AndroidCalendar<AndroidEvent>, values: ContentValues): SubscriptionAndroidEvent =
             SubscriptionAndroidEvent(calendar, values)
     }
