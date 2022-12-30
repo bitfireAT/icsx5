50f1b0205d7a874ec138ecbb6ffd05d8301847e8
Arnau Mora <arnyminer.z@gmail.com>
2022-12-30 14:35:14 +0100

    Added `blockingUi`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/at/bitfire/icsdroid/ThreadUtils.kt
+++ app/src/main/java/at/bitfire/icsdroid/ThreadUtils.kt
@@ -4,11 +4,7 @@ import androidx.annotation.UiThread
 import androidx.annotation.WorkerThread
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.*
 
 /**
  * Runs the code in [block] asynchronously, in the IO thread.
@@ -24,8 +20,18 @@ fun doAsync(@WorkerThread block: suspend CoroutineScope.() -> Unit) = CoroutineS
  * @author Arnau Mora
  * @since 20221227
  * @param block The block of code to be run.
+ * @return The result of [block].
  */
 suspend fun <T> ui(@UiThread block: suspend CoroutineScope.() -> T): T = withContext(Dispatchers.Main, block)
+
+/**
+ * Runs the code in [block] in the UI thread, blocking the current thread.
+ * @author Arnau Mora
+ * @since 20221230
+ * @param block The block of code to be run.
+ * @return The result of [block].
+ */
+fun <T> blockingUi(@UiThread block: suspend CoroutineScope.() -> T): T = runBlocking(Dispatchers.Main, block)
 
 /**
  * Runs the given block of code in the IO thread using the view model scope ([ViewModel.viewModelScope]).
