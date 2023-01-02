e9e10f53778f725f403a768e4e333127738867c4
Ricki Hirner <hirner@bitfire.at>
2023-01-02 18:29:06 +0100

    Make AppDatabase singleton creation more explicit

--- app/src/main/java/at/bitfire/icsdroid/db/AppDatabase.kt
+++ app/src/main/java/at/bitfire/icsdroid/db/AppDatabase.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import at.bitfire.icsdroid.db.AppDatabase.Companion.getInstance
 import at.bitfire.icsdroid.db.dao.EventsDao
 import at.bitfire.icsdroid.db.dao.SubscriptionsDao
 import at.bitfire.icsdroid.db.entity.Subscription
@@ -14,21 +15,37 @@ import at.bitfire.icsdroid.db.entity.SubscriptionEvent
  */
 @Database(entities = [Subscription::class, SubscriptionEvent::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
+
     companion object {
         @Volatile
-        private var INSTANCE: AppDatabase? = null
+        private var instance: AppDatabase? = null
 
         /**
-         * Gets or instantiates a reference to the database's singleton.
-         * @param context The application's context.
-         * @return An instance of [AppDatabase].
+         * Gets or instantiates the database singleton. Thread-safe.
+         * @param context The application's context, required to create the database.
          */
-        fun getInstance(context: Context) = INSTANCE ?: synchronized(this) {
-            INSTANCE ?: Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "icsx5").build().also {  INSTANCE = it }
+        fun getInstance(context: Context): AppDatabase {
+            // if we have an existing instance, return it
+            instance?.let {
+                return it
+            }
+
+            // multiple threads might access this code at once, so synchronize it
+            synchronized(AppDatabase) {
+                // another thread might just have created an instance
+                instance?.let {
+                    return it
+                }
+
+                // create a new instance and save it
+                val db = Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "icsx5").build()
+                instance = db
+                return db
+            }
         }
     }
 
     abstract fun subscriptionsDao(): SubscriptionsDao
-
     abstract fun eventsDao(): EventsDao
+
 }
