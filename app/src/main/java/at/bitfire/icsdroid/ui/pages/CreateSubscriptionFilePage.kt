b34f81d6d419b8e2030006de29a62e42758c4b3f
Arnau Mora <arnyminer.z@gmail.com>
2023-01-03 23:16:18 +0100

    Added pages
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ app/src/main/java/at/bitfire/icsdroid/ui/pages/CreateSubscriptionFilePage.kt
@@ -0,0 +1,9 @@
+package at.bitfire.icsdroid.ui.pages
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CreateSubscriptionFilePage() {
+    Text(text = "file")
+}
