becf7b11fa012a2e20bb37450e73955bc32d5ccd
Arnau Mora <arnyminer.z@gmail.com>
2023-01-03 23:15:03 +0100

    Added `AndroidViewModel.getString`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/at/bitfire/icsdroid/utils/ContextUtils.kt
+++ app/src/main/java/at/bitfire/icsdroid/utils/ContextUtils.kt
@@ -1,11 +1,13 @@
 package at.bitfire.icsdroid.utils
 
+import android.app.Application
 import android.content.Context
 import android.content.res.Resources.NotFoundException
 import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.annotation.UiThread
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.AndroidViewModel
 
 /**
  * Makes a new toast, and shows it.
@@ -62,3 +64,14 @@ fun Fragment.toast(@StringRes textRes: Int, duration: Int = Toast.LENGTH_SHORT):
 @UiThread
 fun Fragment.toast(text: String, duration: Int = Toast.LENGTH_SHORT): Toast =
     Toast.makeText(requireContext(), text, duration).also { it.show() }
+
+/**
+ * Returns a localized formatted string from the application's package's default string table,
+ * substituting the format arguments as defined in [java.util.Formatter] and [String.format].
+ * @param resId Resource id for the format string
+ * @param formatArgs The format arguments that will be used for substitution.
+ * @return The string data associated with the resource, formatted and stripped of styled text
+ * information.
+ */
+fun AndroidViewModel.getString(@StringRes resId: Int, vararg formatArgs: Any) =
+    getApplication<Application>().getString(resId, formatArgs)
