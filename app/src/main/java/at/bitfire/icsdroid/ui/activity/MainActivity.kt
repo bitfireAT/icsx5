afdf68e920b3483a093e543dad65b5d6f6e404d6
Arnau Mora <arnyminer.z@gmail.com>
2023-01-03 22:26:48 +0100

    Fixed animations
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/at/bitfire/icsdroid/ui/activity/MainActivity.kt
+++ app/src/main/java/at/bitfire/icsdroid/ui/activity/MainActivity.kt
@@ -3,7 +3,9 @@ package at.bitfire.icsdroid.ui.activity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.LaunchedEffect
@@ -55,7 +57,33 @@ class MainActivity : AppCompatActivity() {
         setContentThemed {
             val navController = rememberAnimatedNavController()
             AnimatedNavHost(navController, startDestination = Paths.Subscriptions.route) {
-                composable(Paths.Subscriptions) { SubscriptionsScreen(navController, model) }
+                composable(
+                    Paths.Subscriptions,
+                    enterTransition = {
+                        when (initialState.destination.route) {
+                            Paths.Create.route -> slideIntoContainer(
+                                AnimatedContentScope.SlideDirection.Right,
+                                animationSpec = tween(400)
+                            )
+                            else -> slideIntoContainer(
+                                AnimatedContentScope.SlideDirection.Left,
+                                animationSpec = tween(400)
+                            )
+                        }
+                    },
+                    exitTransition = {
+                        when (targetState.destination.route) {
+                            Paths.Create.route -> slideOutOfContainer(
+                                AnimatedContentScope.SlideDirection.Left,
+                                animationSpec = tween(400)
+                            )
+                            else -> slideOutOfContainer(
+                                AnimatedContentScope.SlideDirection.Right,
+                                animationSpec = tween(400)
+                            )
+                        }
+                    }
+                ) { SubscriptionsScreen(navController, model) }
                 composable(Paths.Subscription) { entry ->
                     val id = entry.arguments?.getLong("id") ?: run {
                         toast(stringResource(R.string.could_not_load_calendar))
