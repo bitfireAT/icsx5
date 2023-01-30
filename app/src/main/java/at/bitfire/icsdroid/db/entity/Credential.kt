32c0300e9bf2a3bfd292144b2bd8d8d336b8ed28
Arnau Mora <arnyminer.z@gmail.com>
2023-01-30 18:10:42 +0100

    Added foreign key
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/at/bitfire/icsdroid/db/entity/Credential.kt
+++ app/src/main/java/at/bitfire/icsdroid/db/entity/Credential.kt
@@ -2,18 +2,20 @@ package at.bitfire.icsdroid.db.entity
 
 import androidx.lifecycle.LiveData
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
 /**
  * Stores the credentials to be used with a specific subscription.
- *
- * FIXME Should we allow null username and/or password?
  * @param subscriptionId The id of the subscription that matches this credential.
  * @param username The username of the credential.
  * @param password The password of the credential.
  */
 @Entity(
-    tableName = "credentials"
+    tableName = "credentials",
+    foreignKeys = [
+        ForeignKey(entity = Subscription::class, parentColumns = ["id"], childColumns = ["subscriptionId"], onDelete = ForeignKey.CASCADE),
+    ],
 )
 data class Credential(
     @PrimaryKey val subscriptionId: Long,
