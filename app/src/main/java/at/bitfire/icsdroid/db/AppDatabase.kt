3317d4d69f95bf22d6dbf24d94d3ab3530185e2c
Arnau Mora <arnyminer.z@gmail.com>
2023-01-30 16:42:41 +0100

    Removed unused method
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/at/bitfire/icsdroid/db/AppDatabase.kt
+++ app/src/main/java/at/bitfire/icsdroid/db/AppDatabase.kt
@@ -1,7 +1,6 @@
 package at.bitfire.icsdroid.db
 
 import android.content.Context
-import androidx.annotation.VisibleForTesting
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
@@ -22,15 +21,6 @@ abstract class AppDatabase : RoomDatabase() {
     companion object {
         @Volatile
         private var instance: AppDatabase? = null
-
-        /**
-         * This function is only intended to be used by tests, use [getInstance], it initializes
-         * the instance automatically.
-         */
-        @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
-        fun setInstance(instance: AppDatabase?) {
-            this.instance = instance
-        }
 
         /**
          * Gets or instantiates the database singleton. Thread-safe.
