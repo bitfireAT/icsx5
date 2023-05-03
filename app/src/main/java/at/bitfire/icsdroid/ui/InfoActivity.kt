91bf8100a9ab3e270e4284ac0b65c4def6e9fa47
Arnau Mora <arnyminerz@proton.me>
2023-05-03 17:01:24 +0200

    Added header
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- app/src/main/java/at/bitfire/icsdroid/ui/InfoActivity.kt
+++ app/src/main/java/at/bitfire/icsdroid/ui/InfoActivity.kt
@@ -14,11 +14,36 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
+import at.bitfire.icsdroid.BuildConfig
 import at.bitfire.icsdroid.Constants
 import at.bitfire.icsdroid.R
 import com.google.accompanist.themeadapter.material.MdcTheme
@@ -59,15 +84,110 @@ class InfoActivity: AppCompatActivity() {
     }
 
     class LibsFragment: Fragment() {
+        @Composable
+        fun TextDialog(@StringRes descriptionRes: Int, onDismissRequest: () -> Unit) {
+            AlertDialog(
+                onDismissRequest = onDismissRequest,
+                confirmButton = {
+                    TextButton(onClick = onDismissRequest) {
+                        Text(stringResource(android.R.string.ok))
+                    }
+                },
+                text = {
+                    Text(
+                        text = stringResource(descriptionRes),
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+            )
+        }
+
         override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
         ): View = ComposeView(requireContext()).apply {
             setContent {
+                val context = LocalContext.current
+
                 MdcTheme {
+                    var showingLicenseDialog by remember { mutableStateOf(false) }
+                    if (showingLicenseDialog)
+                        TextDialog(R.string.app_info_gplv3_note) { showingLicenseDialog = false }
+
+                    var showingDonateDialog by remember { mutableStateOf(false) }
+                    if (showingDonateDialog)
+                        TextDialog(R.string.donate_message) { showingDonateDialog = false }
+                    
                     LibrariesContainer(
-                        modifier = Modifier.fillMaxSize()
+                        modifier = Modifier.fillMaxSize(),
+                        header = {
+                            // Main Header
+                            item {
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(vertical = 12.dp),
+                                    horizontalArrangement = Arrangement.Center
+                                ) {
+                                    Image(
+                                        bitmap = context.applicationInfo
+                                            .loadIcon(context.packageManager)
+                                            .toBitmap()
+                                            .asImageBitmap(),
+                                        contentDescription = null,
+                                        modifier = Modifier.size(72.dp)
+                                    )
+                                }
+                                Text(
+                                    text = stringResource(R.string.app_name),
+                                    style = MaterialTheme.typography.h5,
+                                    textAlign = TextAlign.Center,
+                                    color = MaterialTheme.colors.onBackground,
+                                    modifier = Modifier.fillMaxWidth()
+                                )
+                                Text(
+                                    text = stringResource(
+                                        R.string.app_info_version,
+                                        BuildConfig.VERSION_NAME,
+                                        BuildConfig.FLAVOR
+                                    ),
+                                    style = MaterialTheme.typography.subtitle1,
+                                    textAlign = TextAlign.Center,
+                                    color = MaterialTheme.colors.onBackground.copy(
+                                        alpha = ContentAlpha.medium
+                                    ),
+                                    modifier = Modifier.fillMaxWidth()
+                                )
+                            }
+                            // Buttons
+                            item {
+                                Row {
+                                    OutlinedButton(
+                                        onClick = { showingLicenseDialog = true },
+                                        modifier = Modifier.weight(1f).padding(horizontal = 4.dp)
+                                    ) { Text(stringResource(R.string.app_info_gplv3)) }
+
+                                    if (BuildConfig.FLAVOR != "gplay")
+                                        OutlinedButton(
+                                            onClick = { showingDonateDialog = true },
+                                            modifier = Modifier.weight(1f).padding(horizontal = 4.dp)
+                                        ) { Text(stringResource(R.string.app_info_donate)) }
+                                }
+                            }
+                            // Description
+                            item {
+                                Text(
+                                    text = stringResource(R.string.app_info_description),
+                                    style = MaterialTheme.typography.subtitle2,
+                                    textAlign = TextAlign.Center,
+                                    color = MaterialTheme.colors.onBackground.copy(
+                                        alpha = ContentAlpha.medium
+                                    ),
+                                    modifier = Modifier.fillMaxWidth()
+                                )
+                            }
+                        }
                     )
                 }
             }
