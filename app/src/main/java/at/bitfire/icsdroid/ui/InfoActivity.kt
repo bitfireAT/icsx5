eb0dde677c64440afaf867f8ce552c4e33cb14be
Arnau Mora <Arnyminer.Z@gmail.com>
2023-05-08 19:59:30 +0200

    Jetpack Compose - Phase I (#140)
    * Completed Phase I
    
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>
    
    * Upgraded Kotlin to `1.8.10`
    
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>
    
    * Migrated `Transformations.map`
    
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>
    
    * Made `useDarkTheme` nullable
    
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>
    
    * Moved version check
    
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>
    
    * Removed custom theme
    
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>
    
    * Optimized and updated dependencies
    
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>
    
    * Using Compose-based AboutLibraries
    
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>
    
    * Added header
    
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>
    
    * Updated compose compiler
    
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>
    
    * Moved info header to its own function
    
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>
    
    * Simplified layout
    
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>
    
    * Increased logo padding
    
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>
    
    * Use ComponentActivity; small layout changes
    
    ---------
    
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>
    Co-authored-by: Ricki Hirner <hirner@bitfire.at>

--- app/src/main/java/at/bitfire/icsdroid/ui/InfoActivity.kt
+++ app/src/main/java/at/bitfire/icsdroid/ui/InfoActivity.kt
@@ -9,57 +9,67 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.graphics.drawable.toBitmap
+import androidx.core.text.HtmlCompat
 import at.bitfire.icsdroid.BuildConfig
 import at.bitfire.icsdroid.Constants
 import at.bitfire.icsdroid.R
-import com.mikepenz.aboutlibraries.Libs
-import com.mikepenz.aboutlibraries.LibsBuilder
+import com.google.accompanist.themeadapter.material.MdcTheme
+import com.mikepenz.aboutlibraries.ui.compose.LibrariesContainer
 
-class InfoActivity: AppCompatActivity() {
+class InfoActivity: ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (savedInstanceState == null) {
-            val builder = LibsBuilder()
-                .withAboutIconShown(true)
-                .withAboutAppName(getString(R.string.app_name))
-                .withAboutDescription(getString(R.string.app_info_description))
-                .withAboutVersionShownName(true)
-                .withAboutVersionString(getString(R.string.app_info_version, BuildConfig.VERSION_NAME, BuildConfig.FLAVOR))
-                .withAboutSpecial1(getString(R.string.app_info_gplv3))
-                .withAboutSpecial1Description(getString(R.string.app_info_gplv3_note))
-                .withLicenseShown(true)
-
-                .withFields(R.string::class.java.fields)
-                .withLibraryModification("org_brotli__dec", Libs.LibraryFields.LIBRARY_NAME, "Brotli")
-                .withLibraryModification("org_brotli__dec", Libs.LibraryFields.AUTHOR_NAME, "Google")
-
-            if (BuildConfig.FLAVOR != "gplay") {
-                builder
-                    .withAboutSpecial2(getString(R.string.app_info_donate))
-                    .withAboutSpecial2Description(getString(R.string.donate_message))
-            }
-
-            supportFragmentManager.beginTransaction()
-                .replace(android.R.id.content, builder.supportFragment())
-                .commit()
+        setContent {
+            MainLayout()
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.app_info_activity, menu)
-        return true
-    }
-
-    fun showWebSite(item: MenuItem) {
+    fun showWebSite() {
         launchUri(Uri.parse("https://icsx5.bitfire.at/?pk_campaign=icsx5-app&pk_kwd=info-activity"))
     }
 
-    fun showTwitter(item: MenuItem) {
+    fun showTwitter() {
         launchUri(Uri.parse("https://twitter.com/icsx5app"))
     }
 
@@ -68,8 +78,139 @@ class InfoActivity: AppCompatActivity() {
         try {
             startActivity(intent)
         } catch (e: ActivityNotFoundException) {
-            Log.w(Constants.TAG, "No browser installed")
+            Toast.makeText(this, getString(R.string.no_browser), Toast.LENGTH_LONG).show()
+            Log.w(Constants.TAG, "No browser to view $uri")
         }
+    }
+
+
+    @Composable
+    @Preview
+    fun MainLayout() {
+        MdcTheme {
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        navigationIcon = {
+                            IconButton({ onNavigateUp() }) {
+                                Icon(
+                                    imageVector = Icons.Filled.ArrowBack,
+                                    contentDescription = null
+                                )
+                            }
+                        },
+                        title = {
+                            Text(
+                                stringResource(R.string.app_name)
+                            )
+                        },
+                        actions = {
+                            IconButton({ showWebSite() }) {
+                                Icon(
+                                    painter = painterResource(R.drawable.ic_public),
+                                    contentDescription = stringResource(R.string.app_info_web_site)
+                                )
+                            }
+                            IconButton({ showTwitter() }) {
+                                Icon(
+                                    painter = painterResource(R.drawable.twitter_white),
+                                    contentDescription = stringResource(R.string.app_info_web_site)
+                                )
+                            }
+                        }
+                    )
+                }
+            ) { contentPadding ->
+                Column(Modifier.padding(contentPadding)) {
+                    Header()
+                    License()
+                    LibrariesContainer()
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun Header() {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val context = LocalContext.current
+
+            Image(
+                bitmap = context.applicationInfo
+                    .loadIcon(context.packageManager)
+                    .toBitmap()
+                    .asImageBitmap(),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(vertical = 12.dp)
+                    .size(72.dp)
+            )
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.h5,
+                color = MaterialTheme.colors.onBackground
+            )
+            Text(
+                text = stringResource(
+                    R.string.app_info_version,
+                    BuildConfig.VERSION_NAME,
+                    BuildConfig.FLAVOR
+                ),
+                style = MaterialTheme.typography.subtitle1,
+                color = MaterialTheme.colors.onBackground,
+                modifier = Modifier.alpha(ContentAlpha.medium)
+            )
+        }
+    }
+
+    @Composable
+    fun License() {
+        val showLicenseDialog = rememberSaveable { mutableStateOf(false) }
+        if (showLicenseDialog.value)
+            TextDialog(R.string.app_info_gplv3_note, showLicenseDialog)
+
+        val showDonateDialog = rememberSaveable { mutableStateOf(false) }
+        if (showDonateDialog.value)
+            TextDialog(R.string.donate_message, showDonateDialog)
+
+        Row {
+            OutlinedButton(
+                onClick = { showLicenseDialog.value = true },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 4.dp)
+            ) {
+                Text(stringResource(R.string.app_info_gplv3))
+            }
+            OutlinedButton(
+                onClick = { showDonateDialog.value = true },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 4.dp)
+            ) {
+                Text(stringResource(R.string.app_info_donate))
+            }
+        }
+    }
+
+    @Composable
+    fun TextDialog(@StringRes text: Int, state: MutableState<Boolean>, buttons: @Composable () -> Unit = {}) {
+        AlertDialog(
+            text = {
+                AndroidView({ context ->
+                   TextView(context).also {
+                       it.text = HtmlCompat.fromHtml(
+                           getString(text).replace("\n", "<br/>"),
+                           HtmlCompat.FROM_HTML_MODE_COMPACT)
+                   }
+                }, modifier = Modifier.verticalScroll(rememberScrollState()))
+            },
+            buttons = buttons,
+            onDismissRequest = { state.value = false }
+        )
     }
 
 }
