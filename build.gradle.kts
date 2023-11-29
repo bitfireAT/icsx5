92a650bc798d0b91649c501bcc120046a5714030
Sunik Kupfer <kupfer@bitfire.at>
2023-11-29 14:32:11 +0100

    Update project dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 buildscript {
     val aboutLibsVersion by extra("10.7.0")
-    val agpVersion = "8.1.1"
-    val kotlinVersion = "1.9.0"
-    val kspVersion = "1.0.11"
+    val agpVersion = "8.1.4"
+    val kotlinVersion = "1.9.20"
+    val kspVersion = "1.0.14"
 
     repositories {
         google()
