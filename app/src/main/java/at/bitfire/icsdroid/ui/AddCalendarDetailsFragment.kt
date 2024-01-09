23976e18a3d1140e8855c409db9088d96d27591b
Sunik Kupfer <kupfer@bitfire.at>
2024-01-09 13:47:31 +0100

    Extract SubscriptionModel

--- app/src/main/java/at/bitfire/icsdroid/ui/AddCalendarDetailsFragment.kt
+++ app/src/main/java/at/bitfire/icsdroid/ui/AddCalendarDetailsFragment.kt
@@ -4,10 +4,7 @@
 
 package at.bitfire.icsdroid.ui
 
-import android.app.Application
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
@@ -22,20 +19,11 @@ import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
-import androidx.lifecycle.viewModelScope
-import at.bitfire.icsdroid.Constants
 import at.bitfire.icsdroid.R
-import at.bitfire.icsdroid.SyncWorker
-import at.bitfire.icsdroid.db.AppDatabase
-import at.bitfire.icsdroid.db.entity.Credential
-import at.bitfire.icsdroid.db.entity.Subscription
 import at.bitfire.icsdroid.model.CredentialsModel
+import at.bitfire.icsdroid.model.SubscriptionModel
 import at.bitfire.icsdroid.model.SubscriptionSettingsModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class AddCalendarDetailsFragment: Fragment() {
 
@@ -129,63 +117,5 @@ class AddCalendarDetailsFragment: Fragment() {
             true
         } else
             false
-
-
-    class SubscriptionModel(application: Application) : AndroidViewModel(application) {
-
-        private val database = AppDatabase.getInstance(getApplication())
-        private val subscriptionsDao = database.subscriptionsDao()
-        private val credentialsDao = database.credentialsDao()
-
-        val success = MutableLiveData(false)
-        val errorMessage = MutableLiveData<String>()
-
-        /**
-         * Creates a new subscription taking the data from the given models.
-         */
-        fun create(
-            subscriptionSettingsModel: SubscriptionSettingsModel,
-            credentialsModel: CredentialsModel,
-        ) {
-            viewModelScope.launch(Dispatchers.IO) {
-                try {
-                    val subscription = Subscription(
-                        displayName = subscriptionSettingsModel.title.value!!,
-                        url = Uri.parse(subscriptionSettingsModel.url.value),
-                        color = subscriptionSettingsModel.color.value,
-                        ignoreEmbeddedAlerts = subscriptionSettingsModel.ignoreAlerts.value ?: false,
-                        defaultAlarmMinutes = subscriptionSettingsModel.defaultAlarmMinutes.value,
-                        defaultAllDayAlarmMinutes = subscriptionSettingsModel.defaultAllDayAlarmMinutes.value,
-                    )
-
-                    /** A list of all the ids of the inserted rows */
-                    val id = subscriptionsDao.add(subscription)
-
-                    // Create the credential in the IO thread
-                    if (credentialsModel.requiresAuth.value == true) {
-                        // If the subscription requires credentials, create them
-                        val username = credentialsModel.username.value
-                        val password = credentialsModel.password.value
-                        if (username != null && password != null) {
-                            val credential = Credential(
-                                subscriptionId = id,
-                                username = username,
-                                password = password
-                            )
-                            credentialsDao.create(credential)
-                        }
-                    }
-
-                    // sync the subscription to reflect the changes in the calendar provider
-                    SyncWorker.run(getApplication())
-
-                    success.postValue(true)
-                } catch (e: Exception) {
-                    Log.e(Constants.TAG, "Couldn't create calendar", e)
-                    errorMessage.postValue(e.localizedMessage)
-                }
-            }
-        }
-    }
 
 }
