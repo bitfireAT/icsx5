aeadb4d4e873e3272a0ee15bfcbe7d4164cc748d
Arnau Mora <arnyminerz@proton.me>
2024-02-28 10:31:15 +0100

    Added copyright information
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- app/src/main/java/at/bitfire/icsdroid/ui/InfoActivity.kt
+++ app/src/main/java/at/bitfire/icsdroid/ui/InfoActivity.kt
@@ -38,6 +38,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.graphics.drawable.toBitmap
@@ -164,6 +165,14 @@ class InfoActivity: ComponentActivity() {
                 style = MaterialTheme.typography.titleMedium,
                 color = MaterialTheme.colorScheme.onBackground,
                 fontWeight = FontWeight.Normal
+            )
+            Text(
+                text = stringResource(R.string.app_info_copyright),
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.onBackground,
+                fontWeight = FontWeight.Normal,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp)
             )
         }
     }
