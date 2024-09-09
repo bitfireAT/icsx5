6889fb9e3ba579ede4b44fead4ceefdcf77c9f30
Arnau Mora <Arnyminer.Z@gmail.com>
2024-09-09 11:30:05 +0200

    Increase SDK to 35 (#410)
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,14 +10,14 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
 
     namespace = "at.bitfire.icsdroid"
 
     defaultConfig {
         applicationId = "at.bitfire.icsdroid"
         minSdk = 23
-        targetSdk = 34
+        targetSdk = 35
 
         versionCode = 80
         versionName = "2.2.4"
