6e762d5f03cda900dbc2291733ba951a5d531b10
Arnau Mora Gras <arnyminerz@proton.me>
2024-09-17 16:09:44 +0200

    Message can't be null
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- app/src/main/java/at/bitfire/icsdroid/ui/partials/ActionCard.kt
+++ app/src/main/java/at/bitfire/icsdroid/ui/partials/ActionCard.kt
@@ -20,7 +20,7 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun ActionCard(
     title: String,
-    message: String?,
+    message: String,
     actionText: String,
     modifier: Modifier = Modifier,
     onAction: () -> Unit
@@ -49,13 +49,11 @@ fun ActionCard(
                     Text(text = actionText.uppercase())
                 }
             }
-            if (message != null) {
-                Text(
-                    text = message,
-                    modifier = Modifier.fillMaxWidth(),
-                    style = MaterialTheme.typography.bodyLarge
-                )
-            }
+            Text(
+                text = message,
+                modifier = Modifier.fillMaxWidth(),
+                style = MaterialTheme.typography.bodyLarge
+            )
         }
     }
 }
