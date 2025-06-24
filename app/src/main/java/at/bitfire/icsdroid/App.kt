0ac7d2bbd97b7bf7d21203bc965ef8e1fe29816a
Sunik Kupfer <kupfer@bitfire.at>
2025-06-24 09:42:16 +0200

    Setup hilt for workmanager

--- app/src/main/java/at/bitfire/icsdroid/App.kt
+++ app/src/main/java/at/bitfire/icsdroid/App.kt
@@ -1,7 +1,20 @@
 package at.bitfire.icsdroid
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class App: Application()
+class App : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+
+}
