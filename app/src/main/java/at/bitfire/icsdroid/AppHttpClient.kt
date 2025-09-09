7bfaec42832c325216f0c5bde533f8ea01f9cd83
Sunik Kupfer <kupfer@bitfire.at>
2025-09-09 13:56:29 +0200

    Fix cert4android not showing user decision dialog

--- app/src/main/java/at/bitfire/icsdroid/AppHttpClient.kt
+++ app/src/main/java/at/bitfire/icsdroid/AppHttpClient.kt
@@ -13,12 +13,11 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.UserAgent
 import kotlinx.coroutines.flow.MutableStateFlow
-import okhttp3.Interceptor
-import okhttp3.Response
 import okhttp3.brotli.BrotliInterceptor
 import okhttp3.internal.tls.OkHostnameVerifier
-import java.util.concurrent.TimeUnit
 import javax.net.ssl.SSLContext
 
 class AppHttpClient @AssistedInject constructor(
@@ -34,18 +33,7 @@ class AppHttpClient @AssistedInject constructor(
             createEngine: (CustomCertManager, SSLContext) -> HttpClientEngine = { certManager, sslContext ->
                 OkHttp.create {
                     addNetworkInterceptor(BrotliInterceptor)
-                    addNetworkInterceptor(object : Interceptor {
-                        override fun intercept(chain: Interceptor.Chain): Response {
-                            val request = chain.request().newBuilder()
-                                .header("User-Agent", customUserAgent ?: Constants.USER_AGENT)
-                                .build()
-                            return chain.proceed(request)
-                        }
-                    })
                     config {
-                        followRedirects(true)
-                        connectTimeout(20, TimeUnit.SECONDS)
-                        readTimeout(60, TimeUnit.SECONDS)
                         sslSocketFactory(sslContext.socketFactory, certManager)
                         hostnameVerifier(certManager.HostnameVerifier(OkHostnameVerifier))
                     }
@@ -64,5 +52,21 @@ class AppHttpClient @AssistedInject constructor(
     }
 
     val httpClient = HttpClient(createEngine(certManager, sslContext))
+        .config {
+            // Add user given user agent to all engines
+            install(UserAgent) {
+                agent = customUserAgent ?: Constants.USER_AGENT
+            }
+
+            // Increase default timeouts
+            install(HttpTimeout) {
+                connectTimeoutMillis = 20_000
+                requestTimeoutMillis = 60_000
+                socketTimeoutMillis = 60_000
+            }
+
+            // Disable redirect following, it's handled by CalendarFetcher
+            followRedirects = false
+        }
 
 }
