85fbcb24f4fb0ccbe1c265a01c1a7994b5ecd1a3
Arnau Mora <arnyminerz@proton.me>
2025-09-28 16:22:47 +0200

    Remove unnecessary padding
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- app/src/main/java/at/bitfire/icsdroid/ui/screen/AddSubscriptionScreen.kt
+++ app/src/main/java/at/bitfire/icsdroid/ui/screen/AddSubscriptionScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
@@ -239,7 +238,6 @@ fun AddSubscriptionScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
-                .imePadding()
         ) { page ->
             when (page) {
                 0 -> EnterUrlComposable(
