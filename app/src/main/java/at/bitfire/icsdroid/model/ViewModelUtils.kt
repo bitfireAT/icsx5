0441c304c1994fd7dbad1f159778c5dc0d1f8f01
Arnau Mora <arnyminerz@proton.me>
2025-09-29 14:09:33 +0200

    Added `ToastDuration` annotation
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- app/src/main/java/at/bitfire/icsdroid/model/ViewModelUtils.kt
+++ app/src/main/java/at/bitfire/icsdroid/model/ViewModelUtils.kt
@@ -2,14 +2,19 @@ package at.bitfire.icsdroid.model
 
 import android.content.Context
 import android.widget.Toast
+import androidx.annotation.IntDef
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+
+@Retention(AnnotationRetention.SOURCE)
+@IntDef(Toast.LENGTH_SHORT, Toast.LENGTH_LONG)
+annotation class ToastDuration
 
 suspend fun toastAsync(
     context: Context,
     message: String?,
     cancelToast: Toast? = null,
-    duration: Int = Toast.LENGTH_LONG,
+    @ToastDuration duration: Int = Toast.LENGTH_LONG,
 ): Toast? = withContext(Dispatchers.Main) {
     cancelToast?.cancel()
 
